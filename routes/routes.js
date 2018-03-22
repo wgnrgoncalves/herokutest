908b56ca2e497ec4ba0d87a8197c66e504d5629d
Wagner Gonçalves <wagner@ar.inf.br>
2018-03-22 07:23:40 -0300

    Versao com Rest3

--- routes/routes.js
+++ routes/routes.js
@@ -1,42 +1,53 @@
 var faker = require("faker");
+let unidades = require('./unidades.json');
+
 var appRouter = function (app) {
-    app.get("/api/teste", function(req, res) {
-      res.status(200).send("Welcome to our restful API");
+    app.get("/api/teste", function (req, res) {
+        res.status(200).send("Welcome to our restful API");
     });
     app.get('/', (req, res) => res.render('pages/index'));
 
-    app.get("/api/users", function (req, res) {
-        var data = ({
-          firstName: faker.name.firstName(),
-          lastName: faker.name.lastName(),
-          username: faker.internet.userName(),
-          email: faker.internet.email()
-        });
-        res.status(200).send(data);
-      });
-    
-     app.get("/api/users/:num", function (req, res) {
-       var users = [];
-       var num = req.params.num;
-    
-       if (isFinite(num) && num  > 0 ) {
-         for (i = 0; i <= num-1; i++) {
-           users.push({
-               firstName: faker.name.firstName(),
-               lastName: faker.name.lastName(),
-               username: faker.internet.userName(),
-               email: faker.internet.email()
-            });
-         }
-    
-         res.status(200).send(users);
+    app.get("/api/producao/unidades", function (req, res) {
         
-       } else {
-         res.status(400).send({ message: 'invalid number supplied' });
-       }
+        res.status(200).send(unidades);
     });
 
 
-  }
-  
-  module.exports = appRouter;
+    app.get("/api/users", function (req, res) {
+        var data = ({
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            username: faker.internet.userName(),
+            email: faker.internet.email()
+        });
+        res.status(200).send(data);
+    });
+
+
+
+
+    app.get("/api/users/:num", function (req, res) {
+        var users = [];
+        var num = req.params.num;
+
+        if (isFinite(num) && num > 0) {
+            for (i = 0; i <= num - 1; i++) {
+                users.push({
+                    firstName: faker.name.firstName(),
+                    lastName: faker.name.lastName(),
+                    username: faker.internet.userName(),
+                    email: faker.internet.email()
+                });
+            }
+
+            res.status(200).send(users);
+
+        } else {
+            res.status(400).send({ message: 'invalid number supplied' });
+        }
+    });
+
+
+}
+
+module.exports = appRouter;
